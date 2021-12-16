8a17deab3acd6c8b437cb951fda3322e15ec2a1f
abubaker <mohammad_abubaker@yahoo.com>
2021-12-17 01:10:21 +0500

    Working...

--- app/src/main/java/com/example/notificationsdemo/MainActivity.kt
+++ app/src/main/java/com/example/notificationsdemo/MainActivity.kt
@@ -85,6 +85,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         // 3. Set up main Intent for notification that is the Activity that you want to launch when user tap on notification.
         val mainIntent = Intent(this, MainActivity::class.java)
+        
         // Gets a PendingIntent containing the intent.
         val mainPendingIntent = PendingIntent.getActivity(
             this,
@@ -102,44 +103,59 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             notificationChannelId
         )
 
-        notificationCompatBuilder // INBOX_STYLE sets title and content for API 16+ (4.1 and after) when the
-            // notification is expanded.
-            .setStyle(inboxStyle) // Title for API <16 (4.0 and below) devices and API 16+ (4.1 and after) when the
-            // notification is collapsed.
-            .setContentTitle(Content.mContentTitle) // Content for API <24 (7.0 and below) devices and API 16+ (4.1 and after) when the
-            // notification is collapsed.
+        notificationCompatBuilder
+
+            // INBOX_STYLE sets title and content for API 16+ (4.1 and after) when the notification is expanded.
+            .setStyle(inboxStyle)
+
+            // Title for API <16 (4.0 and below) devices and API 16+ (4.1 and after) when the notification is collapsed.
+            .setContentTitle(Content.mContentTitle)
+
+            // Content for API <24 (7.0 and below) devices and API 16+ (4.1 and after) when the notification is collapsed.
             .setContentText(Content.mContentText)
+
             .setSmallIcon(R.drawable.ic_stat_notification)
+
             .setLargeIcon(
                 BitmapFactory.decodeResource(
                     resources,
                     R.drawable.ic_person
                 )
             )
+
             .setContentIntent(mainPendingIntent)
-            .setDefaults(NotificationCompat.DEFAULT_ALL) // Set primary color (important for Wear 2.0 Notifications).
+
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+
+            // Set primary color (important for Wear 2.0 Notifications).
             .setColor(
                 ContextCompat.getColor(
                     applicationContext,
                     R.color.purple_500
                 )
-            ) // SIDE NOTE: Auto-bundling is enabled for 4 or more notifications on API 24+ (N+)
+            )
+
+            // SIDE NOTE: Auto-bundling is enabled for 4 or more notifications on API 24+ (N+)
             // devices and all Wear devices. If you have more than one notification and
             // you prefer a different summary notification, set a group key and create a
             // summary notification via
             // .setGroupSummary(true)
             // .setGroup(GROUP_KEY_YOUR_NAME_HERE)
+
             // Sets large number at the right-hand side of the notification for API <24 devices.
             .setSubText(Content.mNumberOfNewEmails.toString())
-            .setCategory(Notification.CATEGORY_EMAIL) // Sets priority for 25 and below. For 26 and above, 'priority' is deprecated for
+            .setCategory(Notification.CATEGORY_EMAIL)
+
+            // Sets priority for 25 and below. For 26 and above, 'priority' is deprecated for
             // 'importance' which is set in the NotificationChannel. The integers representing
             // 'priority' are different from 'importance', so make sure you don't mix them.
-            .setPriority(Content.mPriority) // Sets lock-screen visibility for 25 and below. For 26 and above, lock screen
+            .setPriority(Content.mPriority)
+
+            // Sets lock-screen visibility for 25 and below. For 26 and above, lock screen
             // visibility is set in the NotificationChannel.
             .setVisibility(Content.mChannelLockscreenVisibility)
 
-        // If the phone is in "Do not disturb mode, the user will still be notified if
-        // the sender(s) is starred as a favorite.
+        // If the phone is in "Do not disturb mode, the user will still be notified if the sender(s) is starred as a favorite.
         for (name in Content.mEmailSenders()) {
             notificationCompatBuilder.addPerson(name)
         }
